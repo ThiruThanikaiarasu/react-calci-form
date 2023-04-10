bd2017412fea6b04cc52db3ecdae1dc1c67a5f15
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-04-10 15:24:28 +0530

    Add. Added Portfolio and Calculator Component.

--- src/components/router/CalculatorComponent/CalculatorComponent.jsx
+++ src/components/router/CalculatorComponent/CalculatorComponent.jsx
@@ -1,11 +1,59 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export class CalculatorComponent extends Component {
-  render() {
-    return (
-      <div>CalculatorComponent</div>
-    )
+// export class CalculatorComponent extends Component {
+//   render() {
+//     return (
+//       <div>CalculatorComponent</div>
+//     )
+//   }
+// }
+
+// export default CalculatorComponen
+
+import React, { useState } from 'react';
+
+function CalculatorComponent() {
+  const [input, setInput] = useState('');
+
+  function handleButtonClick(e) {
+    const value = e.target.value;
+    setInput(input + value);
   }
+
+  function handleClearButtonClick() {
+    setInput('');
+  }
+
+  function handleCalculateButtonClick() {
+    const result = eval(input);
+    setInput(result.toString());
+  }
+
+  return (
+    <div className="calculator flex flex-col justify-center items-center h-screen">
+
+      <div className="buttons grid grid-cols-4 gap-2 p-4 w-min-[0px]">
+      <div className="display bg-gray-900 text-white text-right p-4 text-4xl font-bold col-span-4">{input || '0'}</div>      
+        <button value="7" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>7</button>
+        <button value="8" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>8</button>
+        <button value="9" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>9</button>
+        <button value="/" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-full" onClick={handleButtonClick}>÷</button>
+        <button value="4" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>4</button>
+        <button value="5" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>5</button>
+        <button value="6" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>6</button>
+        <button value="*" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-full" onClick={handleButtonClick}>x</button>
+        <button value="1" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>1</button>
+        <button value="2" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>2</button>
+        <button value="3" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>3</button>
+        <button value="-" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-full" onClick={handleButtonClick}>-</button>
+        <button value="0" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg col-span-2" onClick={handleButtonClick}>0</button>
+        <button value="." className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>.</button>
+        <button value="+" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-full" onClick={handleButtonClick}>+</button>
+        <button value="=" className="bg-orange-400 text-white font-bold text-2xl p-2 rounded-full col-span-2" onClick={handleCalculateButtonClick}>=</button>
+        <button value="C" className="bg-gray-300 text-black font-bold text-xl w-[50%] p-1 rounded-full col-span-2" onClick={handleClearButtonClick}>C</button> 
+        </div>
+      </div>
+  )
 }
 
 export default CalculatorComponent
--- src/components/router/PortfolioComponent/PortfolioComponent.jsx
+++ src/components/router/PortfolioComponent/PortfolioComponent.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from 'react';
+import myImage from '../../img/thiru.jpg'
 
 function PortfolioComponent() {
   return (
-    <div>PortfolioComponent</div>
-  )
-}
+    <div className="">
+      <header
+        className='bg-black text-white text-center text-6xl'
+      >
+        <div 
+          className='py-10'
+        >
+          <h1>Thirunavukkarasu T</h1>
+          {/* <h2>STUDENT</h2> */}
+        </div>
+      </header>
 
-export default PortfolioComponent
+      <section
+        className='bg-slate-300 float-right w-[80%] h-screen p-7'
+      >
+        <div>
+          <div className="section-heading text-5xl">
+            <h2>Profile</h2>
+          </div>
+
+          <p
+            className='text-2xl my-7'
+          >
+            I am a web development student who is looking forward to my first work experience. I consider myself as a responsible person and I am excited to develop this skill further in a professional setting. I believe that my skills and attitude will be an asset to any company and I am eager to learn and grow in this field.</p>
+
+          <div className="section-heading text-5xl">
+            <h2>Skills</h2>
+          </div>
+          <div className="skills-content">
+            <ul className='py-7'>
+              <li>
+                <h4 className='text-2xl pl-5'>React</h4>
+                <span className="bar"><span className="react"></span></span>
+              </li>
+              <li>
+                <h4 className='text-2xl pl-5'>Java</h4>
+                <span className="bar"><span className="java"></span></span>
+              </li>
+              <li>
+                <h4 className='text-2xl pl-5'>MongoDB</h4>
+                <span className="bar"><span className="mongodb"></span></span>
+              </li>
+              <li>
+                <h4 className='text-2xl pl-5'>NodeJs</h4>
+                <span className="bar"><span className="nodejs"></span></span>
+              </li>
+            </ul>
+          </div>
+          <div className="section-heading text-5xl mt-15">
+            <h2>Internship</h2>
+          </div>
+          <p className='text-2xl pt-7'>I recently completed an internship at ConnectInfoSystem, and it was an amazing experience! I gained so much knowledge and hands-on experience in the field of information technology. I was able to work on various projects with experienced professionals, and I learned a lot about the inner workings of a successful IT company. I am very grateful for the opportunity to have interned at ConnectInfoSystem, and I would highly recommend it to anyone interested in pursuing a career in IT.</p>
+        </div>
+      </section>
+
+      <aside
+        className='bg-yellow-500 h-screen px-7'
+      >
+          <img src={myImage} alt="Passport-size-photo" 
+            className='object-contain w-[17%] rounded-[50%] pt-24'
+          />
+           
+        
+        <div className="sidebar-content">
+          <br />
+          <div className="text-2xl font-bold pt-20">
+            <h3><i className="fa fa-user" aria-hidden="true"></i>CONTACT</h3>
+          </div>
+
+          <p className='text-lg'>7845098854</p>
+          <p className='text-lg'>thiru.thanikaiarasu@gmail.com</p>
+          <p className='text-lg'>123,4th Anna St. Avadi, Chennai</p>
+          <p className='text-lg'><a href="https://twitter.com/ThiruThan "></a></p>
+        </div>
+
+      </aside>
+    </div>
+  )}
+
+  export default PortfolioComponent
